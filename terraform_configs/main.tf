ddd38730fe17f57c815c4245ad5ba0f118b53554
pravinraj-marimuthu <marimuthupravinraj@gmail.com>
2025-10-14 15:45:32 +0530

    modify app

--- terraform_configs/main.tf
+++ terraform_configs/main.tf
@@ -36,6 +36,7 @@ module "eks" {
   cluster_version = "1.33"
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
   eks_managed_node_groups = {
     default = {
       ami_type = "AL2023_x86_64_STANDARD"
